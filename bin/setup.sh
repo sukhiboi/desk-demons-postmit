20919252659fde26271dda95562d891fa9a9c060
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-07-18 23:27:08 +0530

    |#5|sukhiboi/naveen|created setup.sh
    What changes you have made?
    - created setup.sh will will install all the dependencies and create git hooks

---
+++ bin/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+npm install
+cat <<EOF > .git/hooks/pre-commit
+npm test
+if [ \$? != 0 ]; then
+    exit 1
+fi
+EOF
+chmod +x .git/hooks/pre-commit
+cat <<EOF > .git/hooks/pre-push
+npm run lint
+if [ \$? != 0 ]; then
+    exit 1
+fi
+EOF
+chmod +x .git/hooks/pre-push
