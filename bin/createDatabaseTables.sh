37047f7958a59728f6453875e8afa9c985377ffe
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-07-22 17:59:16 +0530

    - extracted common script to create database tables

---
+++ bin/createDatabaseTables.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+sqlite3 $1 <<'END_SQL'
+
+CREATE TABLE IF NOT EXISTS users (
+	username VARCHAR(39) PRIMARY KEY,
+	github_username VARCHAR(39) UNIQUE NOT NULL,
+	name VARCHAR(100),
+	bio VARCHAR(200),
+	dob DATE,
+	joined_date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+	id NUMBER primary key,
+	username VARCHAR(39),
+	message VARCHAR(180) NOT NULL,
+	posted_at datetime NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS responses (
+	post_id NUMBER NOT NULL,
+	response_id NUMBER PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS likes (
+	post_id NUMBER NOT NULL,
+	username VARCHAR(39) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS reposts (
+	post_id NUMBER NOT NULL,
+	username VARCHAR(39) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS bookmarks (
+	post_id NUMBER NOT NULL,
+	username VARCHAR(39) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS followers (
+	username VARCHAR(39) NOT NULL,
+	follower VARCHAR(39) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS hashtags (
+	post_id NUMBER NOT NULL,
+	hashtag VARCHAR(10) NOT NULL
+);
+END_SQL
--- bin/setupTestDatabase.sh
+++ bin/setupTestDatabase.sh
@@ -1,62 +1,10 @@
 #! /bin/bash
 
-mkdir -p test/database;
+mkdir -p test/database
+databasePath=./test/database/testDatabase.db
+./bin/createDatabaseTables.sh $databasePath
 
-sqlite3 ./test/database/testDatabase.db <<'END_SQL'
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-	username VARCHAR(39) PRIMARY KEY,
-	github_username VARCHAR(39) UNIQUE NOT NULL,
-	name VARCHAR(100),
-	bio VARCHAR(200),
-	dob DATE,
-	joined_date DATE NOT NULL
-);
-
-DROP TABLE IF EXISTS posts;
-CREATE TABLE posts (
-	id NUMBER primary key,
-	username VARCHAR(39),
-	message VARCHAR(180) NOT NULL,
-	posted_at datetime NOT NULL
-);
-
-DROP TABLE IF EXISTS responses;
-CREATE TABLE responses (
-	post_id NUMBER NOT NULL,
-	response_id NUMBER PRIMARY KEY
-);
-
-DROP TABLE IF EXISTS likes;
-CREATE TABLE likes (
-	post_id NUMBER NOT NULL,
-	username VARCHAR(39) NOT NULL
-);
-
-DROP TABLE IF EXISTS reposts;
-CREATE TABLE reposts (
-	post_id NUMBER NOT NULL,
-	username VARCHAR(39) NOT NULL
-);
-
-DROP TABLE IF EXISTS bookmarks;
-CREATE TABLE bookmarks (
-	post_id NUMBER NOT NULL,
-	username VARCHAR(39) NOT NULL
-);
-
-DROP TABLE IF EXISTS followers;
-CREATE TABLE followers (
-	username VARCHAR(39) NOT NULL,
-	follower VARCHAR(39) NOT NULL
-);
-
-DROP TABLE IF EXISTS hashtags;
-CREATE TABLE hashtags (
-	post_id NUMBER NOT NULL,
-	hashtag VARCHAR(10) NOT NULL
-);
-
+sqlite3 $databasePath <<'END_SQL'
 .mode csv
 .import "|tail -n +2 ./test/data/usersData.csv" users
 .import "|tail -n +2 ./test/data/postsData.csv" posts
