234c8397e4375b223cf40fa489448bcae458ec04
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-07-23 14:11:42 +0530

    |#17|vaishnavi/sukhiboi| Created GetUserDetails method
    Created getUserDetails method in DBClient class

--- test/testDBClient.js
+++ test/testDBClient.js
@@ -1,4 +1,5 @@
 const { assert } = require('chai');
+const sinon = require('sinon');
 const { DBClient } = require('../lib/DBClient');
 
 describe('DBClient', () => {
@@ -25,6 +26,34 @@ describe('DBClient', () => {
       } catch (err) {
         assert.deepStrictEqual(err, expected);
       }
+    });
+  });
+
+  describe('getUserDetails', () => {
+    it('should resolve the user details of a valid userId', () => {
+      const expectedUserDetail = {
+        id: 1,
+        username: 'sukhiboi',
+        name: 'sukhdev',
+      };
+      const getStub = sinon.stub().yields(null, expectedUserDetail);
+      const client = new DBClient({ get: getStub });
+      const userId = 1;
+      client.getUserDetails(userId).then((userDetails) => {
+        assert.deepStrictEqual(userDetails, expectedUserDetail);
+        sinon.assert.calledOnce(getStub);
+      });
+    });
+
+    it('should reject giving user details for invalid userId', () => {
+      const expectedError = new Error('userId not found');
+      const getStub = sinon.stub().yields(expectedError, null);
+      const client = new DBClient({ get: getStub });
+      const userId = 1;
+      client.getUserDetails(userId).catch((error) => {
+        assert.equal(error, expectedError);
+        sinon.assert.calledOnce(getStub);
+      });
     });
   });
 });
