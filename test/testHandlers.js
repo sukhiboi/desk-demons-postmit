b87c560a4cd02b6962dc2ec42c94fbe443c29673
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-24 09:21:38 +0530

    |#16|naveen/noora| used sinon in handlers tests

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -1,15 +1,23 @@
 const request = require('supertest');
+const sinon = require('sinon');
 const { app } = require('../lib/app');
 
 describe('Handlers', () => {
   context('Request for Home Page', () => {
     it('Should serve the Home Page with Posts', done => {
       const userDetails = { name: 'john', username: 'john' };
-      const dbClient = {};
-      dbClient.getPosts = () => Promise.resolve([{ id: 1 }]);
-      dbClient.getUserDetails = () => Promise.resolve(userDetails);
-      app.locals.dbClient = dbClient;
-      request(app).get('/').expect(200).expect(/john/, done);
+      const getPosts = sinon.stub().resolves([{ id: 1, user_id: 1 }]);
+      const getUserDetails = sinon.stub().resolves(userDetails);
+      app.locals.dbClient = { getPosts, getUserDetails };
+      request(app)
+        .get('/')
+        .expect(200)
+        .expect(() => {
+          sinon.assert.calledOnce(getPosts);
+          sinon.assert.calledOnce(getUserDetails);
+          sinon.assert.calledOnceWithExactly(getUserDetails, 1);
+        })
+        .expect(/john/, done);
     });
   });
 });
