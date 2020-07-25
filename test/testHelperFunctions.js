ef989975f2a1f8016ce6262a40ee09aa5a2c3c02
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-25 15:57:25 +0530

    |#12|naveen/noora| added tests for parseTimeStamp

--- test/testHelperFunctions.js
+++ test/testHelperFunctions.js
@@ -1,4 +1,4 @@
-const { extractInitials } = require('./../lib/helperFunctions');
+const { extractInitials, parseTimeStamp } = require('./../lib/helperFunctions');
 const { assert } = require('chai');
 
 describe('#Helper Functions', () => {
@@ -11,6 +11,16 @@ describe('#Helper Functions', () => {
     });
     it('should extract the initials when name is more than 2 word long', () => {
       assert.equal(extractInitials('john samuel brad'), 'JS');
+    });
+  });
+
+  describe('parseTimeStamp()', () => {
+    it('it should give a few seconds ago for current time', () => {
+      assert.strictEqual(parseTimeStamp(new Date()), 'a few seconds ago');
+    });
+    it('it should give formatted date for date before/after 24hrs', () => {
+      const timestamp = new Date('2020-07-24');
+      assert.strictEqual(parseTimeStamp(timestamp), 'Jul 24, 2020');
     });
   });
 });
