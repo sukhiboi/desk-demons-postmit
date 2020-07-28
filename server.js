194543fee54e67c35958719114315b3adc3571e3
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-28 11:20:11 +0530

    |#13|naveen/sukhiboi| created Auth model
    created Auth model
    adding Auth model instance to app.locals and using from there

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const sqlite3 = require('sqlite3').verbose();
 const DBClient = require('./lib/DBClient');
 const expressApp = require('./lib/expressApp');
 const App = require('./lib/app');
+const Auth = require('./lib/auth');
 
+const clientId = 'a10adde55f64586894bf';
+const clientSecret = '260e6be0ce6751f5557dda1192b1a3fa5b18993b';
 const db = new sqlite3.Database('./database/postmitDatabase.db');
 expressApp.locals.app = new App(new DBClient(db));
+expressApp.locals.auth = new Auth(clientId, clientSecret);
 
 const [, , PORT] = process.argv;
 expressApp.listen(PORT, () => process.stdout.write(`listening on ${PORT}\n`));
