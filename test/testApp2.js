4fb67146ace62047c894e349b6ebd48588615403
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-07-30 19:05:06 +0530

    |deskdemons| added tests for getUserFeed method in app class

--- test/testApp2.js
+++ test/testApp2.js
@@ -12,9 +12,16 @@ describe('#App', () => {
     return [{ postId: postId, userId, postedAt: new Date(), message: 'hi' }];
   };
 
+  const createApp = function (datastore) {
+    const app = new App(datastore);
+    app.userId = userId;
+    app.username = userDetails.username;
+    app.fullName = userDetails.name;
+    return app;
+  };
   // const [dummyPost] = createDummyPosts();
 
-  // const expectedTableError = new Error('Error: Table not found');
+  const expectedTableError = new Error('Error: Table not found');
   const expectedUserDetailsError = new Error('Error: Invalid userId');
 
   describe('updateUser()', () => {
@@ -47,7 +54,7 @@ describe('#App', () => {
     it('should update the given posts with required details', async () => {
       const getUserDetailsStub = sinon.stub().resolves(userDetails);
       const getAllPostLikersStub = sinon.stub().resolves([{ userId }]);
-      const app = new App({
+      const app = createApp({
         getUserDetails: getUserDetailsStub,
         getAllPostLikers: getAllPostLikersStub,
       });
@@ -70,14 +77,77 @@ describe('#App', () => {
       sinon.assert.calledOnceWithExactly(getUserDetailsStub, userId);
       sinon.assert.calledOnceWithExactly(getAllPostLikersStub, postId);
     });
+
     it('should reject any error', async () => {
       const getUserDetailsStub = sinon.stub().rejects(expectedUserDetailsError);
-      const app = new App({ getUserDetails: getUserDetailsStub });
+      const app = createApp({ getUserDetails: getUserDetailsStub });
       try {
         await app.updatePost(userId, createDummyPosts());
       } catch (err) {
         assert.deepStrictEqual(err, expectedUserDetailsError);
         sinon.assert.calledOnceWithExactly(getUserDetailsStub, userId);
+      }
+    });
+  });
+
+  describe('getUserFeed()', () => {
+    it('should resolve to the feedsposts of user', async () => {
+      const getFollowingStub = sinon.stub().resolves([{ userId: 2 }]);
+      const getUserPostsStub = sinon.stub().resolves(createDummyPosts());
+      const getUserDetailsStub = sinon.stub().resolves(userDetails);
+      const getAllPostLikersStub = sinon.stub().resolves([{ userId }]);
+      const app = createApp({
+        getFollowing: getFollowingStub,
+        getUserPosts: getUserPostsStub,
+        getUserDetails: getUserDetailsStub,
+        getAllPostLikers: getAllPostLikersStub,
+      });
+      const expected = {
+        initials: 'JS',
+        loggedUser: 'john',
+        posts: [
+          {
+            initials: 'JS',
+            isDeletable: true,
+            isLiked: true,
+            likedUsers: [{ userId: 1 }],
+            message: 'hi',
+            name: 'john samuel',
+            postId: 1,
+            postedAt: 'a few seconds ago',
+            userId: 1,
+            username: 'john',
+          },
+          {
+            initials: 'JS',
+            isDeletable: true,
+            isLiked: true,
+            likedUsers: [{ userId: 1 }],
+            message: 'hi',
+            name: 'john samuel',
+            postId: 1,
+            postedAt: 'Invalid date',
+            userId: 1,
+            username: 'john',
+          },
+        ],
+      };
+      const actual = await app.getUserFeed();
+      assert.deepStrictEqual(actual, expected);
+      sinon.assert.calledOnceWithExactly(getFollowingStub, userId);
+      sinon.assert.calledTwice(getUserPostsStub);
+      sinon.assert.calledTwice(getUserDetailsStub);
+      sinon.assert.calledTwice(getAllPostLikersStub);
+    });
+    
+    it('should reject any error', async () => {
+      const getFollowingStub = sinon.stub().rejects(expectedTableError);
+      const app = createApp({ getFollowing: getFollowingStub });
+      try {
+        await app.getUserFeed();
+      } catch (err) {
+        assert.deepStrictEqual(err, expectedTableError);
+        sinon.assert.calledOnceWithExactly(getFollowingStub, userId);
       }
     });
   });
